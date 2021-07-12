c17573ab6726a0601584f0123be959ab0c90a9d3
Caleb Anderson <buyananderson@gmail.com>
2021-07-12 18:14:43 +0200

    Correcting linter errors

---
+++ README copy.md
@@ -0,0 +1,57 @@
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+
+
+## Built With
+
+This project is build with:
+
+- HTML;
+- CSS;
+- JavaScript.
+
+
+## Getting Started
+
+To get the content of this project locally you need to run this command in your terminal:
+
+- `https://github.com/andersonlebon/HTM-CSS-CAPSTON_PROJECT.git`
+
+## Test
+
+For tracking linter errors locally you need to follow these steps:
+
+- After cloning the project you need to run this command
+
+  > `npm install`
+  > This command will download all the dependancies of the project
+
+- For tracking the linter errors in HTML file run:
+
+  > `npx hint .`
+
+- For tracking the linter errors in CSS file run:
+  > `npx stylelint "\*_/_.{css,scss}"`
+
+- And For tracking the linter errors in JavaScript file run:
+
+  > `npx eslint .`
+## Author
+
+👤 **Anderson Caleb**
+
+- GitHub: [@Anhersonlebon](https://github.com/andersonlebon)
+- Twitter: [@BuyananaCaleb](https://twitter.com/BuyananaCaleb)
+- LinkedIn: [Anderson Caleb](https://www.linkedin.com/in/anderson-caleb-915343209/)
+
+## :handshake: Contributing
+
+Feel free to check the [issues page](https://github.com/andersonlebon/Portfolio/issues)
+
+## Show your support
+
+Give a :star: if you like this project!
+
+## Acknowledgments
+- Design and inspiration by [Cindy Shin on Behance](https://www.behance.net/gallery/29845175/CC-Global-Summit-2015)
+- Thanks to everyone who will get time to check this code especially to code reviewers.
--- index.js
+++ index.js
@@ -1,48 +1,40 @@
 const books = [];
-let list = document.getElementById("list");
-let bookTitle = document.getElementById("title");
-let bookAuthor = document.getElementById("author");
+const list = document.getElementById('list');
+const bookTitle = document.getElementById('title');
+const bookAuthor = document.getElementById('author');
 const buttonClass = document.querySelector('.buttonClass');
-
-
-function addItem(title, author){
-  books.push({title:title.value, author:author.value})
-  list.innerHTML = "";
-  books.forEach(abook => {
-    let book = document.createElement("li");
-    const deleteBtn = document.createElement("button");
-    deleteBtn.innerText = 'REMOVE';
-    book.innerHTML = `<p>${abook.title}</p>
-  <p>${abook.author} </p>`;
-  deleteBtn.id = abook.title;
-  deleteBtn.className =  "removeBtn"
-    list.appendChild(book);
-    book.appendChild(deleteBtn);
-deleteBtn.addEventListener('click', () => {
-    console.log(deleteBtn);
-    if(deleteBtn.id === abook.title) {
-    const index = books.findIndex((rBook) => rBook.title === deleteBtn.id);
-    books.splice(index, 1);
-    console.log(books);
-    list.removeChild(book)
-    saveFormDataToLocalStorage(books);
-    console.log(localStorage.getItem('books'));
-   
-  }
-})
-  })
-}
-
-buttonClass.addEventListener('click',() =>{
-addItem(bookTitle, bookAuthor);
-saveFormDataToLocalStorage(books);
-console.log(localStorage.getItem('books'));
-})
-
 
 function saveFormDataToLocalStorage(books) {
   localStorage.setItem('books', JSON.stringify(books));
 }
 
+function addItem(title, author) {
+  books.push({ title: title.value, author: author.value });
+  list.innerHTML = '';
+  books.forEach((abook) => {
+    const book = document.createElement('li');
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerText = 'REMOVE';
+    book.innerHTML = `<p>${abook.title}</p>
+  <p>${abook.author} </p>`;
+    deleteBtn.id = abook.title;
+    deleteBtn.className = 'removeBtn';
+    list.appendChild(book);
+    book.appendChild(deleteBtn);
+    deleteBtn.addEventListener('click', () => {
+      if (deleteBtn.id === abook.title) {
+        const index = books.findIndex((rBook) => rBook.title === deleteBtn.id);
+        books.splice(index, 1);
+        list.removeChild(book);
+        saveFormDataToLocalStorage(books);
+        console.log(localStorage.getItem('books'));
+      }
+    });
+  });
+}
 
-
+buttonClass.addEventListener('click', () => {
+  addItem(bookTitle, bookAuthor);
+  saveFormDataToLocalStorage(books);
+  console.log(localStorage.getItem('books'));
+});
