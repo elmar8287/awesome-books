6fde1a96e63605e43f2d7deb20811b5ec9b7eb2f
Caleb Anderson <buyananderson@gmail.com>
2021-07-13 10:48:26 +0200

    Add load the books when the page is reloaded

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const books = [];
+let books = [];
+books = JSON.parse(localStorage.getItem('books'));
 const list = document.getElementById('list');
 const bookTitle = document.getElementById('title');
 const bookAuthor = document.getElementById('author');
@@ -6,8 +7,58 @@ const buttonClass = document.querySelector('.buttonClass');
 
 function addItem(title, author) {
   books.push({ title: title.value, author: author.value });
+  saveBooks(books);
   list.innerHTML = '';
-  books.forEach((abook) => {
+  const getData = localStorage.getItem('books');
+  const reloadBooks = JSON.parse(localStorage.getItem('books'));
+  console.log(reloadBooks);
+  reloadBooks.forEach((abook) => {
+    const book = document.createElement('li');
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerText = 'Remove';
+    book.innerHTML = `<p>${abook.title}</p>
+  <p>${abook.author} </p>`;
+    deleteBtn.id = abook.title;
+    deleteBtn.className = 'removeBtn';
+    const br = document.createElement('br');
+    list.appendChild(book);
+    book.appendChild(deleteBtn);
+    list.appendChild(br);
+    deleteBtn.addEventListener('click', () => {
+      if (deleteBtn.id === abook.title) {
+        const index = books.findIndex((rBook) => rBook.title === deleteBtn.id);
+        books.splice(index, 1);
+        list.removeChild(book);
+        saveBooks(books);
+      }
+    });
+  });
+  console.log(books);
+}
+
+buttonClass.addEventListener('click', () => {
+  addItem(bookTitle, bookAuthor);
+});
+
+function saveBooks(books) {
+  localStorage.setItem('books', JSON.stringify(books));
+}
+
+function saveFormDataToLocalStorage(title, author) {
+  const bookList = {
+    title: title.value,
+    author: author.value,
+  };
+  localStorage.setItem('bookList', JSON.stringify(bookList));
+}
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+
+const inputsArray = [titleInput, authorInput];
+// GETTING FORM DATA FROM LOCAL STORAGE
+window.addEventListener('load', () => {
+  const reloadBooks = JSON.parse(localStorage.getItem('books'));
+  reloadBooks.forEach((abook) => {
     const book = document.createElement('li');
     const deleteBtn = document.createElement('button');
     deleteBtn.innerText = 'Remove';
@@ -27,38 +78,20 @@ function addItem(title, author) {
       }
     });
   });
-}
 
-buttonClass.addEventListener('click', () => {
-  addItem(bookTitle, bookAuthor);
-});
-
-function saveFormDataToLocalStorage(title, author) {
-  const bookList = {
-    title: title.value,
-    author: author.value,
-  };
-  localStorage.setItem('bookList', JSON.stringify(bookList));
-}
-const titleInput = document.querySelector('#title');
-const authorInput = document.querySelector('#author');
-
-const inputsArray = [titleInput, authorInput];
-// GETTING FORM DATA FROM LOCAL STORAGE
-window.addEventListener('load', () => {
   if (JSON.parse(localStorage.getItem('bookList'))) {
-    const { title, author } = JSON.parse(
-      localStorage.getItem('bookList'),
-    );
+    const { title, author } = JSON.parse(localStorage.getItem('bookList'));
     titleInput.value = title;
     authorInput.value = author;
   }
-  inputsArray.forEach((input) => input.addEventListener('input', () => {
-    if (input === titleInput) {
-      titleInput.value = input.value;
-    } else if (input === authorInput) {
-      authorInput.value = input.value;
-    }
-    saveFormDataToLocalStorage(titleInput, authorInput);
-  }));
+  inputsArray.forEach((input) => {
+    input.addEventListener('input', () => {
+      if (input === titleInput) {
+        titleInput.value = input.value;
+      } else if (input === authorInput) {
+        authorInput.value = input.value;
+      }
+      saveFormDataToLocalStorage(titleInput, authorInput);
+    });
+  });
 });
