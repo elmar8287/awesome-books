45ae697115b8db680f352a5b18353afe20b510e3
Caleb Anderson <buyananderson@gmail.com>
2021-07-13 12:46:00 +0200

    Set the local storege

--- index.js
+++ index.js
@@ -2,37 +2,41 @@ const list = document.getElementById('list');
 const bookTitle = document.getElementById('title');
 const bookAuthor = document.getElementById('author');
 const buttonClass = document.querySelector('.buttonClass');
+const books = [];
 
 function saveBooks(books) {
   localStorage.setItem('books', JSON.stringify(books));
 }
+saveBooks(books);
 
 function addItem(title, author) {
   const reloadBooks = JSON.parse(localStorage.getItem('books'));
-  reloadBooks.push({ title: title.value, author: author.value });
-  saveBooks(reloadBooks);
-  list.innerHTML = '';
-  reloadBooks.forEach((abook) => {
-    const book = document.createElement('li');
-    const deleteBtn = document.createElement('button');
-    deleteBtn.innerText = 'Remove';
-    book.innerHTML = `<p>${abook.title}</p>
-  <p>${abook.author} </p>`;
-    deleteBtn.id = abook.title;
-    deleteBtn.className = 'removeBtn';
-    const br = document.createElement('br');
-    list.appendChild(book);
-    book.appendChild(deleteBtn);
-    list.appendChild(br);
-    deleteBtn.addEventListener('click', () => {
-      if (deleteBtn.id === abook.title) {
-        const index = reloadBooks.findIndex((rBook) => rBook.title === deleteBtn.id);
-        reloadBooks.splice(index, 1);
-        list.removeChild(book);
-        saveBooks(reloadBooks);
-      }
+  if (reloadBooks !== null) {
+    reloadBooks.push({ title: title.value, author: author.value });
+    saveBooks(reloadBooks);
+    list.innerHTML = '';
+    reloadBooks.forEach((abook) => {
+      const book = document.createElement('li');
+      const deleteBtn = document.createElement('button');
+      deleteBtn.innerText = 'Remove';
+      book.innerHTML = `<p>${abook.title}</p>
+    <p>${abook.author} </p>`;
+      deleteBtn.id = abook.title;
+      deleteBtn.className = 'removeBtn';
+      const br = document.createElement('br');
+      list.appendChild(book);
+      book.appendChild(deleteBtn);
+      list.appendChild(br);
+      deleteBtn.addEventListener('click', () => {
+        if (deleteBtn.id === abook.title) {
+          const index = reloadBooks.findIndex((rBook) => rBook.title === deleteBtn.id);
+          reloadBooks.splice(index, 1);
+          list.removeChild(book);
+          saveBooks(reloadBooks);
+        }
+      });
     });
-  });
+  }
 }
 
 buttonClass.addEventListener('click', () => {
